759c9f8c9cc0f0f4d603e7231b8697cc7a786091
Emil Trevino <aemilius@austin.rr.com>
2025-07-24 01:18:52 +0900

    Delete ciee-game/PlayConfig.gd

--- ciee-game/PlayConfig.gd
+++
@@ -1,8 +0,0 @@
-extends  Node
-
-# Playable area (Rect position, size)
-var playfield_rect: Rect2 = Rect2(Vector2.ZERO, Vector2.ZERO)
-
-func init_fullscreen(vp_size: Vector2):
-	# full viewport as playfield
-	playfield_rect = Rect2(Vector2.ZERO, vp_size)
