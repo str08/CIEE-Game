d45e04f077020c5b0fb569ac1f8ed4eda0c45cad
str08 <154005683+str08@users.noreply.github.com>
2025-07-28 16:20:05 +0900

    Trees Connected 1

--- ciee-game/Scenes/tree.gd
+++ ciee-game/Scenes/tree.gd
@@ -1,40 +1,10 @@
 extends RigidBody2D
 
-func _on_wood_gather_area_area_entered(area):
+func _on_wood_gather_small(area):
 	modulate = Color8(85, 85, 85)
-
-
-func _on_wood_gather_area_2_area_entered(area):
+	
+func _on_wood_gather_medium(area):
 	modulate = Color8(85, 85, 85)
-
-
-func _on_wood_gather_area_3_area_entered(area):
-	modulate = Color8(85, 85, 85)
-
-
-func _on_wood_gather_area_4_area_entered(area):
-	modulate = Color8(85, 85, 85)
-
-
-func _on_wood_gather_area_5_area_entered(area):
-	modulate = Color8(85, 85, 85)
-
-
-func _on_wood_gather_area_6_area_entered(area):
-	modulate = Color8(85, 85, 85)
-
-
-func _on_wood_gather_area_7_area_entered(area):
-	modulate = Color8(85, 85, 85)
-
-
-func _on_wood_gather_area_8_area_entered(area):
-	modulate = Color8(85, 85, 85)
-
-
-func _on_wood_gather_area_9_area_entered(area):
-	modulate = Color8(85, 85, 85)
-
-
-func _on_wood_gather_area_10_area_entered(area):
+	
+func _on_wood_gather_large(area):
 	modulate = Color8(85, 85, 85)
