aaf823b24a5404f8469b9d45702a674e93e9d374
AdnerCabral <adnercabral40@gmail.com>
2025-07-28 08:15:51 -0400

    tree wood

--- ciee-game/Scripts/tree.gd
+++ ciee-game/Scripts/tree.gd
@@ -1,10 +1,16 @@
 extends RigidBody2D
 
 func _on_wood_gather_small(area):
+	PlayerStats.change_wood(1)
+	print(PlayerStats.wood)
 	modulate = Color8(85, 85, 85)
 	
 func _on_wood_gather_medium(area):
+	PlayerStats.change_wood(3)
+	print(PlayerStats.wood)
 	modulate = Color8(85, 85, 85)
 	
 func _on_wood_gather_large(area):
+	PlayerStats.change_wood(5)
+	print(PlayerStats.wood)
 	modulate = Color8(85, 85, 85)
